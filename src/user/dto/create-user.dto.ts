84a85de2eb1ea4c0c864e8811b6ec1acbcb6069a
kumaljaye <157775455+kumaljaye@users.noreply.github.com>
2024-02-07 09:16:43 +0530

    Add files via upload

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString } from "class-validator";
+
+
+export class CreateUserDto {
+    
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastName: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+}
